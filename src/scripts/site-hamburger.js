0e648a9b88d072600b9efde96634b6eff4c49783
Nicholas Gillespie <contact@nicholasgillespie.com>
2024-07-21 11:51:55 +0200

    Working on implementing the site-header nav button.

---
+++ src/scripts/site-hamburger.js
@@ -0,0 +1,27 @@
+// JS Script Import:
+// <slot slot="scripts">
+//     <script src="../scripts/site-hamburger.js"></script>
+// </slot>
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const siteHeaderNav = document.querySelector(".site-header nav");
+//   if (siteHeaderNav) {
+//     // siteHeaderNav.classList.add("u-vh");
+//   }
+// });
+
+console.log("site-hamburger.js loaded");
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   // Select the element with the `data-js="hamburger"` attribute
+//   const buttonHamburger = document.querySelector('[data-js="hamburger"]');
+//   const siteHeaderNavUl = document.querySelector(".site-header nav ul");
+
+//   if (siteHeaderNavUl) {
+//     siteHeaderNavUl.style.display = "none";
+//   }
+
+//   if (buttonHamburger) {
+//     buttonHamburger.style.display = "block";
+//   }
+// });
